37b01852bb67ad9cdf2aef58d42bb70598940f33
Francky <kifer.mada@gmail.com>
2012-02-13 16:37:00 +0300

    First commit

---
+++ convert.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+
+cur_dir = File.absolute_path(__FILE__).sub(File.basename(__FILE__), "")
+
+Dir.unlink("output") if Dir.exist?("output")
+Dir.mkdir "output"
+
+Dir.foreach(cur_dir) do |file|
+	if File.file?(file) && File.extname(file) == '.flv'
+		flv_name = File.join(cur_dir, file)
+
+		mp3_name = file.gsub(/flv|FLV/, 'mp3')
+		mp3_name = File.join(cur_dir, "output", mp3_name)
+
+		`ffmpeg -i "#{flv_name}" "#{mp3_name}"`
+	end
+end
+
+Dir.unlink("output") if Dir.entries("output").length <= 2
+
+p "Conversion done"
