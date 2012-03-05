a57767cc8033e087b368f9d907d66b6f9809ccf4
Francky <kifer.mada@gmail.com>
2012-03-05 18:17:08 +0300

    tried parallelizing the conversion - race condition bug

--- convert.rb
+++ convert.rb
@@ -11,7 +11,11 @@ Dir.foreach(cur_dir) do |file|
 		mp3_name = file.gsub(/flv|FLV/, 'mp3')
 		mp3_name = File.join(cur_dir, "output", mp3_name)
 
-		`ffmpeg -i "#{flv_name}" "#{mp3_name}"`
+    # tried to parallelize the conversion - possible race condition on IO writing, using a mutex would be equal to normal queed conversion
+		# pid = spawn('ffmpeg -i "'+flv_name+'" "'+mp3_name+'"')
+    # Process.detach(pid)
+
+    `ffmpeg -i "#{flv_name}" "#{mp3_name}"`
 	end
 end
 
